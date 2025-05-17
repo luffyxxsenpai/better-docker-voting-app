7f91df074abc8bdc30e75d3ab66ddfbaf31c582c
luffy <luffysenpai661@gmail.com>
2025-05-17 23:56:27 +0530

    FIXED fake vote concurrency

--- fake-vote/generate-votes.sh
+++ fake-vote/generate-votes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ab -n 100 -c 50 -p posta -T "application/x-www-form-urlencoded" http://localhost:8080/
-ab -n 220 -c 50 -p postb -T "application/x-www-form-urlencoded" http://localhost:8080/
+ab -n 100 -c 50 -p post-a -T "application/x-www-form-urlencoded" http://localhost:8080/
+ab -n 220 -c 50 -p post-b -T "application/x-www-form-urlencoded" http://localhost:8080/
 
-ab -n 20 -c 10 -p postb -T "application/x-www-form-urlencoded" http://vote.in/
-ab -n 50 -c 10 -p posta -T "application/x-www-form-urlencoded" http://vote.in/
+ab -n 20 -c 10 -p post-b -T "application/x-www-form-urlencoded" http://vote.in/
+ab -n 50 -c 10 -p post-a -T "application/x-www-form-urlencoded" http://vote.in/
